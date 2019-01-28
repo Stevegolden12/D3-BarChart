4bd80ca2323080e323713c80ba94e91a85acca7e
Steve Golden <sgolden2525@outlook.com>
2019-01-28 01:52:49 -0600

    Plot Points
    Trying to get dataset to be accurately dipected on the svg.

--- js/index.js
+++ js/index.js
@@ -13,71 +13,31 @@ req.onload = function () {
   const scaleY = d3.max(gdp)
 
   var margin = { top: 20, right: 20, bottom: 70, left: 40 },
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+  /*
+  const scale = d3.scaleLinear();
+  scale.domain([d3.min(gdp), d3.max(gdp)])
+  scale.range([30, 800])
 
-  // Parse the date / time
-  var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-
-  var y = d3.scale.linear().range([height, 0]);
-
-  var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%Y-%m-%d"));
-
-  var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);idth = (svgWidth / gdp.length);
-
+  const output = scale(1000);
+  */
   var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+    .style("background-color", "lightblue")
+    .attr("class", "mainSvg")
 
-  d3.csv("bar-data.csv", function (error, data) {
+   d3.select(".mainSvg").selectAll("rect")
+    .data(json.data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("width", 20 + "px")
+    .attr("height", (d) => d[1])
+    .attr("x", (d, i) => i * 21)
+    .attr("y", (d, i) => height - d[1])
 
-    data.forEach(function (d) {
-      d.date = parseDate(d.date);
-      d.value = +d.value;
-    });
-
-    x.domain(data.map(function (d) { return d.date; }));
-    y.domain([0, d3.max(data, function (d) { return d.value; })]);
-
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-      .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)");
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Value ($)");
-
-    svg.selectAll("bar")
-      .data(data)
-      .enter().append("rect")
-      .style("fill", "steelblue")
-      .attr("x", function (d) { return x(d.date); })
-      .attr("width", x.rangeBand())
-      .attr("y", function (d) { return y(d.value); })
-      .attr("height", function (d) { return height - y(d.value); });
-
-  });
 
 
 
