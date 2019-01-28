5df302a120d60199b6d31981fcae6ae235948884
Steve Golden <sgolden2525@outlook.com>
2019-01-28 13:13:40 -0600

    Added plots points and Scales
    Added data points
    
    Started to work o n the scale of the data points to the graph.

--- js/index.js
+++ js/index.js
@@ -8,20 +8,23 @@ req.onload = function () {
 
   /* Finding the max data and value in the API*/
   const date = json.data.map((data) => data[0]);
-  const scaleX = d3.max(date);
   const gdp = json.data.map((data) => data[1]);
-  const scaleY = d3.max(gdp)
+ 
 
   var margin = { top: 20, right: 20, bottom: 70, left: 40 },
     width = 800 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-  /*
-  const scale = d3.scaleLinear();
-  scale.domain([d3.min(gdp), d3.max(gdp)])
-  scale.range([30, 800])
 
-  const output = scale(1000);
+  /*
+  var scale = d3.scaleLinear();
+  scale.domain([d3.min(gdp), d3.max(gdp)])
+  scale.range([40, 780])
   */
+  const yScale = d3.scaleLinear()
+      .domain([d3.min(gdp, (d)=>d),d3.max(gdp, (d)=>d)])
+      .range([480, 20])  
+  
+ 
   var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -35,9 +38,10 @@ req.onload = function () {
     .attr("class", "bar")
     .attr("width", 20 + "px")
     .attr("height", (d) => d[1])
-    .attr("x", (d, i) => i * 21)
-    .attr("y", (d, i) => height - d[1])
+    .attr("x", (d, i) => i )
+    .attr("y", (d, i) => yScale(d[1]-height))
 
+  
 
 
 
